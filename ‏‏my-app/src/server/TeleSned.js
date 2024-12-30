a7f7e1fd58bc033c4a69e4aa7ad387fbdeb0a23d
Aabwshmys <aabwshmys84@gmail.com>
2024-12-30 23:06:58 +0300

    Update TeleSned.js

--- ‏‏my-app/src/server/TeleSned.js
+++ ‏‏my-app/src/server/TeleSned.js
@@ -1,21 +1,26 @@
 import axios from 'axios';
 
-
 export const TeleSned = () => {
-    
     const Send = async (des) => {
-        const text = `مدونة سمسا %0A ${des}`;
-        await axios.post(`https://hooks.slack.com/services/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`,JSON.stringify(text),{
-            withCredentials:false,
-            transformRequest:[(data,headers) =>{
-                delete headers.post["Content-Type"]
-                return text
-            }]
-        })
-    }
-  return {
-    Send,
-}
-}
+        const text = `مدونة سمسا \n ${des}`;
+        try {
+            await axios.post(
+                'https://hooks.slack.com/services/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',
+                { text }, // إرسال النص ضمن كائن
+                {
+                    headers: {
+                        'Content-Type': 'application/json', // تحديد نوع المحتوى
+                    },
+                }
+            );
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
+    };
+
+    return {
+        Send,
+    };
+};
 
 export default TeleSned;
